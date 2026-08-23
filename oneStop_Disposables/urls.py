6933575115ed2d431453335554251e1e5875c5bd
Suhasi2005 <vaghajiyanisuhasi@gmail.com>
2026-08-22 22:05:18 -0600

    Add cart/checkout REST API with DRF, fix Order.user cardinality, add tests

--- oneStop_Disposables/urls.py
+++ oneStop_Disposables/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('account/', include('account.urls')),
+    path('api/', include('app.api_urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
